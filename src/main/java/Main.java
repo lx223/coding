35d0d31d8858b45b8fc23eccd2774e0203103929
Lan Xiao <legend.lan.xiao@gmail.com>
2015-04-28 23:50:17 +0100

    Added in solution for leetcode q46

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,11 @@
-import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 
 /**
  * Created by lanxiao on 26/04/15.
  */
 public class Main {
     static public void main(String[] args) {
-        String myString = "\u0048\u0065\u006C\u006C\u006F World";
-        System.out.println(myString);
-
-        byte[] myBytes = null;
-        try {
-            myBytes = myString.getBytes("UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-            System.exit(-1);
-        }
-
-        for (int i = 0; i < myBytes.length; i++) {
-            System.out.println(myBytes[i]);
-        }
+        int[] a = new int[]{1, 2, 3};
+        System.out.println(Arrays.toString(a));
     }
 }
---
+++ src/main/java/leetcode/medium/Q46.java
@@ -0,0 +1,45 @@
+package leetcode.medium;
+
+import java.util.*;
+
+/**
+ * Created by lanxiao on 28/04/15.
+ */
+public class Q46 {
+    public List<List<Integer>> permute(int[] num) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        //permute(num, new BitSet(num.length), new ArrayDeque<>(), ans);
+        List<Integer> permutation = new ArrayList<Integer>();
+        for (int n : num) permutation.add(n);
+        permute2(permutation, 0, ans);
+        return ans;
+    }
+
+    private void permute(int[] num, BitSet visited, Deque<Integer> permutation, List<List<Integer>> ans) {
+        if (permutation.size() == num.length) {
+            ans.add(new ArrayList<>(permutation));
+        } else {
+            for (int i = 0; i < num.length; i++) {
+                if (visited.get(i) == false) {
+                    permutation.push(num[i]);
+                    visited.set(i);
+                    permute(num, visited, permutation, ans);
+                    visited.clear(i);
+                    permutation.pop();
+                }
+            }
+        }
+    }
+
+    private void permute2(List<Integer> permutation, int begin, List<List<Integer>> ans) {
+        if (begin >= permutation.size()) {
+            ans.add(new ArrayList<>(permutation));
+        } else {
+            for (int i = begin; i < permutation.size(); i++) {
+                Collections.swap(permutation, begin, i);
+                permute2(permutation, begin + 1, ans);
+                Collections.swap(permutation, begin, i);
+            }
+        }
+    }
+}
