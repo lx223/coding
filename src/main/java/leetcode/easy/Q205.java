21ecec97120e2ed8f8500f0475d7f92e3a11b3a7
Lan Xiao <legend.lan.xiao@gmail.com>
2015-04-29 14:12:43 +0100

    Added in a new method to solve q205 which takes advantage of the fact that the exact mapping needs not known

--- src/main/java/leetcode/easy/Q205.java
+++ src/main/java/leetcode/easy/Q205.java
@@ -26,4 +26,21 @@ public class Q205 {
         }
         return true;
     }
+
+    /**
+     * This method used two mappings to map each pair of characters to the same group
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isIsomorphic2(String s, String t) {
+        int[] m1 = new int[256];
+        int[] m2 = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            if (m1[s.charAt(i)] != m2[t.charAt(i)]) return false;
+            m1[s.charAt(i)] = m2[t.charAt(i)] = i + 1;
+        }
+        return true;
+    }
 }
