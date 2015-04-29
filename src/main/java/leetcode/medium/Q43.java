223617bd613532610ac3bb30dbe721f5ea6ca6c5
Lan Xiao <legend.lan.xiao@gmail.com>
2015-04-29 23:51:43 +0100

    Added in the solution for leetcode q43

---
+++ src/main/java/leetcode/medium/Q43.java
@@ -0,0 +1,34 @@
+package leetcode.medium;
+
+/**
+ * Created by lanxiao on 28/04/15.
+ */
+public class Q43 {
+    public String multiply(String num1, String num2) {
+        int n1 = num1.length(), n2 = num2.length();
+        int[] products = new int[n1 + n2];
+        for (int i = n1 - 1; i >= 0; i--) {
+            for (int j = n2 - 1; j >= 0; j--) {
+                int d1 = num1.charAt(i) - '0';
+                int d2 = num2.charAt(j) - '0';
+                products[i + j + 1] += d1 * d2;
+            }
+        }
+        int carry = 0;
+        for (int i = products.length - 1; i >= 0; i--) {
+            int tmp = (products[i] + carry) % 10;
+            carry = (products[i] + carry) / 10;
+            products[i] = tmp;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int num : products) sb.append(num);
+        while (sb.length() != 0 && sb.charAt(0) == '0') sb.deleteCharAt(0);
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+
+    static public void main(String[] args) {
+        String num1 = "111", num2 = "11";
+        Q43 q43 = new Q43();
+        System.out.println(q43.multiply(num1, num2));
+    }
+}
