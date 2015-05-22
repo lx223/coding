c9896ef6d000623f49f590e5525cfca81e8ac29b
Lan Xiao <legend.lan.xiao@gmail.com>
2015-05-22 20:57:57 +0100

    optimised the code for solution Q545A

--- src/main/java/codeforces/Q545A.java
+++ src/main/java/codeforces/Q545A.java
@@ -7,26 +7,20 @@ public class Q545A {
         Scanner in = new Scanner(System.in);
         final int n = in.nextInt();
         boolean[] badCars = new boolean[n];
+        int count = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 int collision = in.nextInt();
-                if (i != j && (badCars[i] == false || badCars[j] == false)) {
-                    if (collision == 3) {
-                        badCars[i] = true;
-                        badCars[j] = true;
-                    }
-                    if (collision == 2) badCars[j] = true;
-                    if (collision == 1) badCars[i] = true;
-                }
+                if (i >= j) continue;
+                if (collision == 1 || collision == 3) badCars[i] = true;
+                if (collision == 2 || collision == 3) badCars[j] = true;
             }
+            if (!badCars[i]) count++;
         }
-        int count = 0;
-        for (boolean badCar : badCars) {
-            if (badCar == false) count++;
-        }
+        //for (boolean car : badCars) count = car ? count : count + 1;
         System.out.println(count);
         for (int i = 0; i < n; i++) {
-            if (badCars[i] == false) {
+            if (!badCars[i]) {
                 System.out.print(i + 1 + " ");
             }
         }
