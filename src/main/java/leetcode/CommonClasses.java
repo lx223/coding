87f121e3764a534bd765b6ad4b6ec0b6fbf22ff4
Lan Xiao <lx223@users.noreply.github.com>
2015-06-22 16:47:31 +0100

    added in TreeNode to common classes

--- src/main/java/leetcode/CommonClasses.java
+++ src/main/java/leetcode/CommonClasses.java
@@ -7,9 +7,20 @@ abstract public class CommonClasses {
     protected class ListNode {
         public ListNode next;
         public int val;
-        public ListNode(int val){
+
+        public ListNode(int val) {
             this.val = val;
         }
 
+    }
+
+    protected class TreeNode {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
     }
 }
