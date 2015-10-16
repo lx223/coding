60ab34407d39011a18b2d92b60fb9d3ba22ad644
Lan Xiao <lx223@users.noreply.github.com>
2015-10-16 01:09:33 +0100

    Add solution to Q588A in codeforces

---
+++ src/main/java/codeforces/Q588A.java
@@ -0,0 +1,63 @@
+package codeforces;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Q588A {
+    static public void main(String[] arg) {
+        InputReader in = new InputReader(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        // Solution here
+        int n = in.nextInt();
+        int minP = Integer.MAX_VALUE, tot = 0;
+        for (int i = 0; i < n; i++) {
+            int a = in.nextInt(), p = in.nextInt();
+            if (minP > p) {
+                minP = p;
+            }
+            tot += minP * a;
+        }
+
+        out.println(tot);
+
+        in.close();
+        out.close();
+    }
+
+    static private class InputReader {
+        private BufferedReader reader;
+        private StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public void close() {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    static private class GeometryUtils {
+        public double epsilon = 1e-8;
+    }
+}
